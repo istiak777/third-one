3bc86957d37071ad71001e4e70c05041f2b2f9e2
Istiak Ahmad <istiakefat99@gmail.com>
2023-07-27 17:46:51 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      screens: {
+        'tablet': '460px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1070px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1380px',
+        // => @media (min-width: 1480px) { ... }
+      },
+    }
+  }
